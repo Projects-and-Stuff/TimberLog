a678d06be5c40acde58cf34b650faa8050544f85
Projects-and-Stuff <support@projectsandstuff.com>
2014-07-09 17:59:50 -0400

    Added GLPv3 License Information

--- unitclasslogbook.pas
+++ unitclasslogbook.pas
@@ -1,5 +1,24 @@
 unit unitClassLogbook;
 
+/////////////////////////////////////////////////////////////////////////////
+//                   This file is part of TimberLog.                       //
+/////////////////////////////////////////////////////////////////////////////
+//  TimberLog is free software: you can redistribute it and/or modify      //
+//  it under the terms of the GNU General Public License as published by   //
+//  the Free Software Foundation, either version 3 of the License, or      //
+//  (at your option) any later version.                                    //
+//                                                                         //
+//  TimberLog is distributed in the hope that it will be useful,           //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//  GNU General Public License for more details.                           //
+//                                                                         //
+//  You should have received a copy of the GNU General Public License      //
+//  along with TimberLog.  If not, see <http://www.gnu.org/licenses/>.     //
+/////////////////////////////////////////////////////////////////////////////
+//                     Copyright 2014 Jack D Linke                         //
+/////////////////////////////////////////////////////////////////////////////
+
 // Class Unit for class Logbook
 
 {$mode objfpc}{$H+}
